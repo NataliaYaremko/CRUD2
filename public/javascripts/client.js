0b4159e0b89c749ff0ec2de6054106547c3d9a6f
NataliaYaremko <nataliakinash@gmail.com>
2018-01-30 16:33:51 +0200

    initial commit

---
+++ public/javascripts/client.js
@@ -0,0 +1,44 @@
+let modal;
+let name;
+let lastname;
+let email;
+let age;
+let form;
+
+window.onload = function () {
+  modal = document.getElementById('myModal');
+  name = document.getElementById('name');
+  lastname = document.getElementById('lastname');
+  email = document.getElementById('email');
+  age = document.getElementById('age');
+  form = document.getElementById('myForm');
+
+};
+
+const addUser = function () {
+  modal.style.display = 'block';
+  name.value = '';
+  lastname.value = '';
+  email.value = '';
+  age.value = '';
+};
+const editUser = function (user) {
+  modal.style.display = 'block';
+  form.action = '/users?_method=PUT';
+  name.value = user.name;
+  lastname.value = user.lastname;
+  email.value = user.email;
+  age.value = user.age;
+};
+
+
+const hideModal = function () {
+  modal.style.display = 'none';
+  console.log('Hello');
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
---
+++ routes/users.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+const router = express.Router();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+let users = require('./../data/users.json');
+
+const urlencodedParser = bodyParser.urlencoded({
+  extended: false,
+});
+/* GET users listing. Existing code
+router.get('/', (req, res, next) => {
+  res.send('respond with a resource');
+}); */
+router.use(methodOverride('_method'));
+// My changes
+router.get('/users', (req, res) => {
+  res.render('users', { users });
+});
+
+router.delete('/users', urlencodedParser, (req, res) => {
+  if (req.params.id) {
+    console.log(`${req.params.id} DELETED`);
+    users = users.filter(el => el.id != req.params.id);
+    res.render('users', { users });
+  } else {
+    res.render('users', { users });
+  }
+});
+
+router.post('/users', urlencodedParser, (req, res) => {
+  const user = {};
+  user.name = req.body.name;
+  user.lastname = req.body.lastname;
+  user.email = req.body.email;
+  user.age = req.body.age;
+  user.id = users.length + 1;
+  users.push(user);
+  res.render('users', { users });
+});
+
+
+router.put('/users', urlencodedParser, (req, res) => {
+  const userToEdit = users.find(user => user.id == req.params.id);
+  userToEdit.name = req.body.name;
+  userToEdit.lastname = req.body.lastname;
+  userToEdit.email = req.body.email;
+  userToEdit.age = req.body.age;
+  res.render('users', { users });
+});
+
+module.exports = router;
